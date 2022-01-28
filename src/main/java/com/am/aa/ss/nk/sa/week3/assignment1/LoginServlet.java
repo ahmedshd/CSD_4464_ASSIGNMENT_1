12abec52fcfb80181d9abbecedd614e83d598352
Abhinav Mittal <abhinavmittal93@gmail.com>
2022-01-27 21:03:30 -0500

    Added logic for Login and handled some scenarios.

--- src/main/java/com/am/aa/ss/nk/sa/week3/assignment1/LoginServlet.java
+++ src/main/java/com/am/aa/ss/nk/sa/week3/assignment1/LoginServlet.java
@@ -1,11 +1,21 @@
 package com.am.aa.ss.nk.sa.week3.assignment1;
 
+/***
+ * 
+ * @author Abhinav
+ * Date 27-01-2022
+ * 
+ * This will authenticate the user's credentials.
+ * 
+ */
+
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class LoginServlet
@@ -19,23 +29,31 @@ public class LoginServlet extends HttpServlet {
      */
     public LoginServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendRedirect("Login.jsp");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		HttpSession session = request.getSession(false);
+		if(email == null || password == null || email.strip().equals("") || password.strip().equals("")) {
+			//save message in session
+			session.setAttribute("message", "<p style=\"color:red;\">Please enter all the values.</p>");
+			response.sendRedirect("Login.jsp");
+		} else if("admin".equalsIgnoreCase(email) && "admin".equals(password)) {
+			
+		} else {
+			session.setAttribute("message", "<p style=\"color:red;\">Email or Password is wrong!</p>");
+			response.sendRedirect("Login.jsp");
+		}
 	}
-
 }
--- src/main/webapp/Login.jsp
+++ src/main/webapp/Login.jsp
@@ -4,9 +4,24 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>Login</title>
 </head>
 <body>
-
+	<form action="login" method="POST">
+		<div style="margin-left: 30%;margin-right: 30%;margin-top: 10%;">
+		<%if(session.getAttribute("message") != null && !session.getAttribute("message").equals("")) {
+		    out.println(session.getAttribute("message"));
+		    session.removeAttribute("message");
+			}
+		%>
+			<label>Email</label><br>
+			<input type="email" name="email" required />
+			<br><br>
+			<label>Password</label><br>
+			<input type="password" name="password" required />
+			<br><br>
+			<input type="submit" value="Login" />
+		</div>
+	</form>
 </body>
 </html>
